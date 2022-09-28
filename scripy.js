a72c2a5860f7a1cee9b5af313dcc71a095059274
zombwie <oliver@onowingerei.com>
2022-09-28 10:07:29 +0200

    hallowen theme

---
+++ scripy.js
@@ -0,0 +1,14 @@
+function toggleTheme() {
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    var theme = document.getElementsByTagName('link')[0];
+
+    // Change the value of href attribute
+    // to change the css sheet.
+    if (theme.getAttribute('href') == 'style.css') {
+        theme.setAttribute('href', 'halow.css');
+    } else {
+        theme.setAttribute('href', 'style.css');
+    }
+}
